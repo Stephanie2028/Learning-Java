7015e4677c2d3c237771e360652c9747310fbd40
Stephanie2028 <Stephaniezhu07@gmail.com>
2024-11-25 02:27:12 -0500

    Your commit message

--- Java101/PracticeProblems.java
+++ Java101/PracticeProblems.java
@@ -13,4 +13,50 @@ public class PracticeProblems {
        }
        return count; 
 	}
-}
+
+    public static int[] reverseArray (int[] arr) {
+        int[] newArray = new int[arr.length];
+        for (int i=0; i < arr.length; i++) {
+            newArray[i] = arr[arr.length - 1 - i];
+            
+        }
+        return newArray; 
+
+    }
+    
+    public static double sumGrid(double[][] grid){
+        double sum = 0;
+
+        for (int r= 0; r < grid.length; r++) {
+            for (int c= 0; c < grid.length; c++ ) {
+                sum = sum + grid[c][r];
+            }
+            
+        }
+
+        return sum;
+    
+    }
+
+
+    public static int fib(int n){
+        if (n==0){
+            return 0;
+        }
+        if (n==1){
+            return 1;
+        }
+        int x = 0;
+        int y = 1;
+        int nth = 0;
+
+        for(int i = 2; i<n; i++){
+            nth = x + y;
+            y += x;
+            x = nth; 
+
+        }
+        return nth;
+
+        }
+    }
