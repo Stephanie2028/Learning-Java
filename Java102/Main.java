5867d3123ce8ce765f5b58d71a3230cc3bed4fbc
Stephanie2028 <Stephaniezhu07@gmail.com>
2024-12-01 20:39:10 -0500

    message

--- Java102/Main.java
+++ Java102/Main.java
@@ -1,6 +1,39 @@
 package Java102;
 
+import java.util.ArrayList;
 
 public class Main {
+        // Practice 7) returnAll
+        public static void main(String[] args) {
+        
+            Libraryitem[] items = {
+            new Book("NAME", "123", "author", 10),
+            new DVD("NAME", "235", 1.0)
+        }; 
+
+        returnAll(items);
+    }
+
+        public static void returnAll(Libraryitem[] items) {
+            for (Libraryitem item : items) {
+                item.returnItem();
+            }
+
+
+    }
+
+    //Practice 8) availableItems
+    public static ArrayList<Libraryitem> availableItems(Libraryitem[] items) {
+        ArrayList<Libraryitem> availableItems = new ArrayList<>();
+
+        for (Libraryitem item : items) {
+            if (item.available()) {
+                availableItems.add(item);
+            }
+        }
+
+        return availableItems;
+
+    }
     
 }
